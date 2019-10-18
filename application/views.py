a8a6670e84ff5336720759ca38fc837f00e81e20
Naseer Dhruvsoft <naseer@dhruvsoft.com>
2019-10-18 18:45:53 +0530

    Create views.py

---
+++ application/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def homepage(Request):
+    return HttpResponse("Hello World! I am a Django application created by Naseer")
