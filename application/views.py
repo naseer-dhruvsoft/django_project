4cb8e57dfcb6d66edae6d5c9be98669c2dd28b18
Naseer Dhruvsoft <naseer@dhruvsoft.com>
2019-10-21 15:34:49 +0530

    Update views.py

--- application/views.py
+++ application/views.py
@@ -3,4 +3,5 @@ from django.http import HttpResponse
 
 # Create your views here.
 def homepage(Request):
-    return HttpResponse("Hello World! I am a Django application created by Naseer. This application is now enabled with auto deployments.")
+    return render(Request,'django_app.html')
+    
